6d12a5c1e483d89a0fd4fbf33631ce404f20877f
Tim Holzer <timiholzer@gmail.com>
2020-10-09 23:21:34 -0600

    got questions to progress

--- js/quiz.js
+++ js/quiz.js
@@ -7,46 +7,46 @@
   var allQuestions = [{
     question: "If a tree falls in the forest, why does it fall?",
     options: ["It tripped over a root", "Gravity", "It took a class on Javascript that was too hard", 'That one scene from The Lion King where Scar says, "long live the king!'],
-    answer: 2
+    answer: a3
   }, {
     question: "How do you spell 'Javascipt'?",
     options: ["Triple Moca Frapachino", "Jabba da Hut", "Javascript", "Fortran"],
-    answer: 2
+    answer: a3
   }, {
     question: "This webpage was written in HTML, CSS, and _____?",
     options: ["Javascript", "Spite", "Pen","English"],
-    answer: 0 || 3
+    answer: a1 || a4
   },{
     question: "What does HTML stand for?",
     options: ["Hyper-Text Markup Language", "Peace Love Unity Respect", "Javascript", "Hamburgers Tators Macoroni Lasagna"],
-    answer: 0
+    answer: a1
   }, {
     question: "What makes this quiz work?",
     options: ["Mice in a series of tubes", "Javascript", "Hopes and Dreams", "A pencil and paper"],
-    answer: 1 || 2
+    answer: a2 || a3
   },{
     question: "2 + 2 =",
     options: ["4", "5", "Blue! No Yell-aaahhhhhh!!!", "Diet Poptarts"],
-    answer: 0
+    answer: a1
   },{
     question: "'2 + 2' == ?",
     options: ["4", "2 + 2", "'2 + 2'", "'4'"],
-    answer: 2
+    answer: a3
   },{
     question: "2 + 2 === ?",
     options: ["4", "2 + 2", "'2 + 2'", "'4'"],
-    answer: 0 || 1
+    answer: a1 || a2
   },{
     question: "4 > 2",
     options: ["True", "False", "True, but in Yellow", "False, but in Yellow"],
-    answer: 0 || 2
+    answer: a1 || a3
   },{
     question: "2 > 2",
     options: ["True", "False", "Javascript", "Neither"],
-    answer: 1
+    answer: a2
     }];
 
-
+    quesCounter = 1;
 // function to toggle an id
     function toggleElement() {
         var x = document.getElementById(toggledId);
@@ -62,6 +62,7 @@
 //starts timer and toggles to the question box
 
 document.getElementById("startButton").addEventListener("click", function(){
+    
     var timer=100;
     timerLeft = document.getElementById("timeLeft");
     //calling the toggle of html
@@ -75,28 +76,76 @@ document.getElementById("startButton").addEventListener("click", function(){
        timer = timer-1;
        timerLeft.innerHTML = "Time Left: " + timer;
        console.log(timer); 
+       
        if (timer < 1){
           // end game function
+          
        }
   }
  timerTick = setInterval (timeMinus, 1000);
-    quesCounter = 0
+    
+    nextQuestion();
+    
+
+
+
+
 //build code to bring up the questions here
-    if(quesCounter < allQuestions.length){
+ function nextQuestion(){
+    if(quesCounter-1 < allQuestions.length){
         questionNumber = document.getElementById("questionNumber");
-        questionNumber.innerHTML = "Question Number: " + (quesCounter +1);
+        questionNumber.innerHTML = "Question number: " + (quesCounter);
         butt1 = document.getElementById("a1");
-        butt1.textContent = allQuestions[quesCounter].options[0];
+        butt1.textContent = allQuestions[quesCounter-1].options[0];
         butt2 = document.getElementById("a2");
-        butt2.textContent = allQuestions[quesCounter].options[1];
+        butt2.textContent = allQuestions[quesCounter-1].options[1];
         butt3 = document.getElementById("a3");
-        butt3.textContent = allQuestions[quesCounter].options[2];
+        butt3.textContent = allQuestions[quesCounter-1].options[2];
         butt4 = document.getElementById("a4");
-        butt4.textContent = allQuestions[quesCounter].options[3];
+        butt4.textContent = allQuestions[quesCounter-1].options[3];
+        questionAsked = document.getElementById("questionAsked");
+        questionAsked.textContent = allQuestions[quesCounter-1].question;
 
-        
-
-
+  }
+}
+console.log(quesCounter);
+  //checking which button is clicked
+        document.getElementById("a1").addEventListener("click", function(){
+            if (this.id == allQuestions[quesCounter].answer) {
+                score + 10;
+            } else {
+                timer - 10;
+            }
+            quesCounter= quesCounter +1;
+            nextQuestion();
+        });
+        document.getElementById("a2").addEventListener("click", function(){
+            if (this.id == allQuestions[quesCounter].answer) {
+                score + 10;
+            } else {
+                timer - 10;
+            }
+            quesCounter= quesCounter +1;
+            nextQuestion();
+        });
+        document.getElementById("a3").addEventListener("click", function(){
+            if (this.id == allQuestions[quesCounter].answer) {
+                score + 10;
+            } else {
+                timer - 10;
+            }
+            quesCounter= quesCounter +1;
+            nextQuestion();
+        });
+        document.getElementById("a4").addEventListener("click", function(){
+            if (this.id == allQuestions[quesCounter].answer) {
+                score + 10;
+            } else {
+                timer - 10;
+            }
+            quesCounter= quesCounter +1;
+            nextQuestion();
+        });
 
 
     //code for when the questions are shown
@@ -104,20 +153,14 @@ document.getElementById("startButton").addEventListener("click", function(){
     // answerButtonId = this.id;
     // console.log(answerButtonId);
 
-    //probably need to define answerbutton id and allquestions.answer
 
-            // if (answerButtonId === allQuestionsanswer ) {
-    //     score + 10;
-    // }
-    // else {
-    //     timer - 10;
     // });
-    }
-    else {
+   // }
+   // else {
         //end game code
-    }
+ //   }
 
-    ;
+
 
 
 
